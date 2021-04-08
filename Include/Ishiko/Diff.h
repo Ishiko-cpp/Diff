6f79149c28ec8ed8ba92b0ef0f9c1f7f4dd802a4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-08 23:49:10 +0100

    Updates

---
+++ Include/Ishiko/Diff.h
@@ -0,0 +1,13 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_DIFF_H_
+#define _ISHIKO_DIFF_H_
+
+#include "BinaryDiff.h"
+#include "TextDiff.h"
+
+#endif
---
+++ Include/Ishiko/Diff/BinaryDiff.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_DIFF_BINARYDIFF_H_
+#define _ISHIKO_DIFF_BINARYDIFF_H_
+
+#endif
