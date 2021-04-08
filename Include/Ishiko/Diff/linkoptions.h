02ebc7eeaa4ba9276a48354a97d42a0e4e1ac614
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-08 23:31:10 +0100

    Update makefiles

---
+++ Include/Ishiko/Diff/linkoptions.h
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2018-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_DIFF_LINKOPTIONS_H_
+#define _ISHIKO_DIFF_LINKOPTIONS_H_
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoDiff-d.lib")
+#else
+#pragma comment(lib, "IshikoDiff.lib")
+#endif
+
+#endif
