c6dbce412409f2c70c0f0bbb86d13f3394762036
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-19 21:03:54 +0100

    Add test files

---
+++ Tests/Source/AlgorithmsTests.cpp
@@ -0,0 +1,14 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+*/
+
+#include "AlgorithmsTests.h"
+
+using namespace Ishiko::Tests;
+
+AlgorithmsTests::AlgorithmsTests(const TestNumber& number, const TestEnvironment& environment)
+    : TestSequence(number, "Algorithms tests", environment)
+{
+}
---
+++ Tests/Source/AlgorithmsTests.h
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_DIFF_TESTS_ALGORITHMSTESTS_H_
+#define _ISHIKO_DIFF_TESTS_ALGORITHMSTESTS_H_
+
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+class AlgorithmsTests : public Ishiko::Tests::TestSequence
+{
+public:
+    AlgorithmsTests(const Ishiko::Tests::TestNumber& number, const Ishiko::Tests::TestEnvironment& environment);
+};
+
+#endif
