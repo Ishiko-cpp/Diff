bf9c4103338b6588ce1d47fd56761ef306d722dc
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-27 21:59:05 +0100

    Updates

--- tests/Source/main.cpp
+++ tests/Source/main.cpp
@@ -1,14 +1,14 @@
 /*
     Copyright (c) 2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+    See https://github.com/ishiko-cpp/diff/blob/main/LICENSE.txt
 */
 
 #include "AlgorithmsTests.h"
 #include "TextChunkTests.h"
 #include "TextPatchTests.h"
 #include "TextDiffTests.h"
-#include <Ishiko/TestFramework/TestFrameworkCore.h>
+#include <Ishiko/Tests/Core.h>
 
 using namespace Ishiko::Tests;
 
