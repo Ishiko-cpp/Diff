011b2d5c584210c104fe70b5d8689d30687e50b2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-27 22:07:08 +0100

    CI updates (#22)

--- tests/Source/TextChunkTests.cpp
+++ tests/Source/TextChunkTests.cpp
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+    See https://github.com/ishiko-cpp/diff/blob/main/LICENSE.txt
 */
 
 #include "TextChunkTests.h"
--- tests/Source/TextPatchTests.h
+++ tests/Source/TextPatchTests.h
@@ -1,13 +1,13 @@
 /*
     Copyright (c) 2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Diff/blob/main/LICENSE.txt
+    See https://github.com/ishiko-cpp/diff/blob/main/LICENSE.txt
 */
 
 #ifndef _ISHIKO_DIFF_TESTS_TEXTPATCHTESTS_H_
 #define _ISHIKO_DIFF_TESTS_TEXTPATCHTESTS_H_
 
-#include <Ishiko/TestFramework/TestFrameworkCore.h>
+#include <Ishiko/Tests/Core.h>
 
 class TextPatchTests : public Ishiko::Tests::TestSequence
 {
