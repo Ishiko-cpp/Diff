cb1fc9f0691d012fc173956f61ed02e6ae700d75
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-06 17:39:58 +0100

    Fix test code build (#50)

--- tests/src/AlgorithmsTests.hpp
+++ tests/src/AlgorithmsTests.hpp
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_DIFF_TESTS_ALGORITHMSTESTS_HPP_
 #define _ISHIKO_CPP_DIFF_TESTS_ALGORITHMSTESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class AlgorithmsTests : public Ishiko::TestSequence
 {
