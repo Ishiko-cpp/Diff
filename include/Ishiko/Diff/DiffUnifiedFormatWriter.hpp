cfa7f105d8f47cdfad6e342be6d4a5157eca666a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-09 11:52:50 +0100

    Add files

---
+++ include/Ishiko/Diff/DiffUnifiedFormatWriter.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/diff/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_DIFF_DIFFUNIFIEDFORMATWRITER_HPP_
+#define _ISHIKO_CPP_DIFF_DIFFUNIFIEDFORMATWRITER_HPP_
+
+#endif
