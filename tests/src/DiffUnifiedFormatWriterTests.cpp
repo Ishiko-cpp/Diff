29b66045a705a6078d6c66494dda41af9cc8b1e7
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-09 12:36:11 +0100

    Add tests

--- tests/src/DiffUnifiedFormatWriterTests.cpp
+++ tests/src/DiffUnifiedFormatWriterTests.cpp
@@ -6,6 +6,8 @@
 
 #include "DiffUnifiedFormatWriterTests.hpp"
 #include "Ishiko/Diff/DiffUnifiedFormatWriter.hpp"
+#include "Ishiko/Diff/TextDiff.hpp"
+#include "Ishiko/Diff/TextPatch.hpp"
 #include <boost/filesystem.hpp>
 
 using namespace Ishiko;
@@ -14,6 +16,7 @@ DiffUnifiedFormatWriterTests::DiffUnifiedFormatWriterTests(const TestNumber& num
     : TestSequence(number, "DiffUnifiedFormatWriter tests", context)
 {
     append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+    append<HeapAllocationErrorsTest>("write test 1", WriteTest1);
 }
 
 void DiffUnifiedFormatWriterTests::ConstructorTest1(Test& test)
@@ -28,5 +31,27 @@ void DiffUnifiedFormatWriterTests::ConstructorTest1(Test& test)
     ISHIKO_TEST_FAIL_IF(error);
     ISHIKO_TEST_FAIL_IF_FILES_NEQ("DiffUnifiedFormatWriterTests_ConstructorTest1.txt",
         "DiffUnifiedFormatWriterTests_ConstructorTest1.txt");
+    ISHIKO_TEST_PASS();
+}
+
+void DiffUnifiedFormatWriterTests::WriteTest1(Test& test)
+{
+    boost::filesystem::path originalFile = test.context().getTestDataPath("EmptyFile1.txt");
+    boost::filesystem::path newFile = test.context().getTestDataPath("EmptyFile2.txt");
+    boost::filesystem::path output =
+        test.context().getTestOutputPath("DiffUnifiedFormatWriterTests_WriteTest1.txt");
+
+    Error error;
+    TextPatch patch = TextDiff::LineDiffFiles(originalFile, newFile, error);
+
+    DiffUnifiedFormatWriter writer(output, error);
+    writer.writeOriginalFilePath(originalFile.string());
+    writer.writeNewFilePath(newFile.string());
+    writer.write(patch);
+    writer.close();
+
+    ISHIKO_TEST_FAIL_IF(error);
+    ISHIKO_TEST_FAIL_IF_FILES_NEQ("DiffUnifiedFormatWriterTests_WriteTest1.txt",
+        "DiffUnifiedFormatWriterTests_WriteTest1.txt");
     ISHIKO_TEST_PASS();
 }
